3893da21cfaa13531420d7f4fa9a8632a913d60b
andy <352605880@qq.com>
2020-01-14 20:00:16 +0800

    CanvasView 没有使用PortDuff

--- knowledge/app/src/main/java/com/andy/knowledge/CanvasView.java
+++ knowledge/app/src/main/java/com/andy/knowledge/CanvasView.java
@@ -2,6 +2,7 @@ package com.andy.knowledge;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -17,21 +18,38 @@ import androidx.annotation.Nullable;
  * ================================================
  */
 public class CanvasView extends View {
+
+    private Paint paint;
+
     public CanvasView(Context context) {
-        super(context);
+        this(context, null);
     }
 
     public CanvasView(Context context, @Nullable AttributeSet attrs) {
-        super(context, attrs);
+        this(context, attrs, 0);
     }
 
     public CanvasView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        paint = new Paint();
+        paint.setAntiAlias(true);
+
     }
 
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        //设置背景色
+        canvas.drawARGB(255, 139, 197, 186);
+
+        int canvasWidth = canvas.getWidth();
+        int r = canvasWidth / 3;
+        //绘制黄色的圆形
+        paint.setColor(0xFFFFCC44);
+        canvas.drawCircle(r, r, r, paint);
+        //绘制蓝色的矩形
+        paint.setColor(0xFF66AAFF);
+        canvas.drawRect(r, r, r * 2.7f, r * 2.7f, paint);
     }
 }
